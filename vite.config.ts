03bc9c3f230e16af0d022de2880d885ede861696
João <joaovictorpfr@gmail.com>
2023-04-20 11:08:02 -0300

    add base

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    fs: {
+      // Allow serving files from one level up to the project root
+      allow: ['..'],
+    },
+  },
+  plugins: [react()],
+})
